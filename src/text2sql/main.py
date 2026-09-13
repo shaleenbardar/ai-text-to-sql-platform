4d87d84e356ab14503919461f4d4101a1b8926be
Shaleen Bardar <shaleen@Shaleens-MacBook-Pro.local>
2026-09-13 22:39:41 +0530

    chore: configure environment variables

--- src/text2sql/main.py
+++ src/text2sql/main.py
@@ -1,5 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
 def main():
-    print("AI Text-to-SQL Platform")
+    environment = os.getenv("APP_ENV", "unknown")
+    print(f"Application environment: {environment}")
+
 
 if __name__ == "__main__":
     main()
